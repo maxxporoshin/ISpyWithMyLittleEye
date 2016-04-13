02704625c726cc640487c15f1008c7ccde75e17a
maxxporoshin <maxxporoshin@gmail.com>
2016-04-13 04:08:37 +0300

    initial commit

---
+++ ISpyWithMyLittleEye/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using System.Collections.Generic;
+
+namespace ISpyWithMyLittleEye
+{
+    [Activity(Label = "I Spy With My Little Eye", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button addSessionButton;
+        ListView sessionList;
+        List<String> sessions;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            sessionList = FindViewById<ListView>(Resource.Id.sessionList);
+            sessions = new List<String>();
+            var adapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, sessions);
+            sessionList.Adapter = adapter;
+            var inflater = (LayoutInflater)GetSystemService(Context.LayoutInflaterService);
+            var view = inflater.Inflate(Resource.Layout.AddSessionDialog, null);
+            var nameEdit = view.FindViewById<TextView>(Resource.Id.sessionNameEdit);
+            var dialogBuilder = new AlertDialog.Builder(this)
+                .SetView(view)
+                .SetPositiveButton("Ok", (sender, args) => {
+                    adapter.Add(nameEdit.Text);
+                    adapter.NotifyDataSetChanged();
+                    nameEdit.Text = "";
+                })
+                .SetNegativeButton("Nope", (sender, args) => {
+                    nameEdit.Text = "";
+                });
+            var dialog = dialogBuilder.Create();
+            addSessionButton = FindViewById<Button>(Resource.Id.addSessionButton);
+            addSessionButton.Click += (sender, args) =>
+            {
+                dialog.Show();
+            };
+            sessionList.ItemClick += (sender, args) => {
+                string item = adapter.GetItem(args.Position);
+                var session = new Intent(this, typeof(SessionActivity));
+                session.Put 
+            };
+        }
+    }
+}
+
